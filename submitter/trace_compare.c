8bab221cfa6a408fa9b06df7a55f4419d7cae4aa
Maxime Martinasso <martinasso@cscs.ch>
2018-02-26 17:42:31 +0100

    Improve trace_compare.

--- submitter/trace_compare.c
+++ submitter/trace_compare.c
@@ -10,17 +10,23 @@
 
 #include "trace.h"
 
+typedef enum field {
+	submit = 0,
+	eligible,
+	start,
+	end,
+	duration
+} fields;
+
+fields f = submit;
+char* field_name = NULL;
 char* workload_trace_file = NULL;
 char* replay_trace_file = NULL;
-double threshold = 0.0;
-int check_sanity = 0;
 char help_msg[] = "\
 list_trace [OPTIONS]\n\
     -w, --wrkldfile filename      The name of the trace file with the original data\n\
     -r, --replay    filename      The name of the trace file with the replayed data\n\
     -f, --field     field         Field to compare trace: duration, submit, eligible, start, end, nodes, state\n\
-    -d, --differ     value        Threshold from which to report a difference\n\
-    -s, --sanity                  Check nodes and state\n\
     -h, --help                    This help message.\n";
 
 void getArgs(int argc, char** argv)
@@ -28,14 +34,13 @@ void getArgs(int argc, char** argv)
     static struct option long_options[] = {
         {"wrkldfile",      1, 0, 'w'},
         {"replay",         1, 0, 'r'},
-        {"differ",          1, 0, 'd'},
-        {"sannity",         0, 0, 's'},
+        {"field",          1, 0, 'f'},
         {"help",           0, 0, 'h'},
     };
     int opt_char, option_index;
 
     while (1) {
-        if ( (opt_char = getopt_long(argc, argv, "w:r:d:hs", long_options,
+        if ( (opt_char = getopt_long(argc, argv, "w:r:f:h", long_options,
                                      &option_index)) == -1 )
             break;
         switch ( opt_char) {
@@ -45,11 +50,23 @@ void getArgs(int argc, char** argv)
         case ('r'):
             replay_trace_file = strdup(optarg);
             break;
-        case ('d'):
-            threshold = strtod(optarg,NULL);
-            break;
-        case ('s'):
-            check_sanity = 1;
+        case ('f'):
+	    field_name = strdup(optarg);
+	    if (strcmp("submit",optarg) == 0) {
+		    f = submit;
+	    }
+	    if (strcmp("eligible",optarg) == 0) {
+		    f = eligible;
+	    }
+	    if (strcmp("start",optarg) == 0) {
+		    f = start;
+	    }
+	    if (strcmp("end",optarg) == 0) {
+		    f = end;
+	    }
+	    if (strcmp("duration",optarg) == 0) {
+		    f = duration;
+	    }
             break;
         case ('h'):
             printf("%s\n", help_msg);
@@ -64,21 +81,31 @@ static inline double euclidean_dist(time_t x, time_t y)
     return sqrt( (x-y)*(x-y) );
 }
 
+int job_compare(const void *s1, const void *s2)
+{
+  job_trace_t *e1 = (job_trace_t*)s1;
+  job_trace_t *e2 = (job_trace_t*)s2;
+  return e1->id_job - e2->id_job;
+}
+
+
 int main(int argc, char *argv[])
 {
     struct stat stat_buf;
     int work_file, replay_file;
     unsigned long long j, min_jobs, nwork_jobs, nreplay_jobs;
-    double dist;
+    double *dist;
     time_t t1, t2;
-    job_trace_t jwt; // job_work_trace
-    job_trace_t jrt; // job_replay_trace
+    job_trace_t *jwt; // job_work_trace
+    job_trace_t *jrt; // job_replay_trace
     time_t jwt_min_start = LONG_MAX;//for makespan
     time_t jwt_max_end = 0; // for makespan
     time_t jrt_min_start = LONG_MAX;//for makespan
     time_t jrt_max_end = 0; // for makespan
     size_t query_length = 0;
     char query[1024];
+    double avg_field,std_field;
+    double cum_field, cum_stddev_field;
 
     getArgs(argc, argv);
     if (workload_trace_file == NULL || replay_trace_file == NULL) {
@@ -108,79 +135,91 @@ int main(int argc, char *argv[])
 
 
     min_jobs = nreplay_jobs;
-    if (nwork_jobs < min_jobs) {
-        min_jobs = nwork_jobs;
-    }
+if (nwork_jobs != nreplay_jobs) {
+    printf("Missmatch of number of jobs: (%llu != %llu). Exiting.\n",nwork_jobs,nreplay_jobs);
+    exit(-1);
+}
+    dist = (double*)malloc(min_jobs*sizeof(double));
+    jwt = (job_trace_t*)malloc(min_jobs*sizeof(job_trace_t));
+    jrt = (job_trace_t*)malloc(min_jobs*sizeof(job_trace_t));
+    read(work_file, jwt, min_jobs*sizeof(job_trace_t));
+    read(replay_file, jrt, min_jobs*sizeof(job_trace_t));
+
+    qsort(jwt, min_jobs, sizeof(job_trace_t), job_compare);
+    qsort(jrt, min_jobs, sizeof(job_trace_t), job_compare);
 
     for(j = 0; j < min_jobs; j++) {
-        read(work_file, &jwt, sizeof(job_trace_t));
-        read(replay_file, &jrt, sizeof(job_trace_t));
 
-        if (jwt.id_job != jrt.id_job) {
-            printf("Missmatch of job id (%d != %d), files are out of synch. Exiting.\n",jwt.id_job, jrt.id_job);
+        if (jwt[j].id_job != jrt[j].id_job) {
+            printf("Missmatch of job id (%d != %d), files are out of synch. Exiting.\n",jwt[j].id_job, jrt[j].id_job);
             exit(-1);
         }
 
-        if (check_sanity) {
-            if ( jwt.nodes_alloc != jrt.nodes_alloc ) {
-                printf("[%d] Number of nodes differ: %d <> %d\n",jwt.id_job, jwt.nodes_alloc, jrt.nodes_alloc);
-            }
-            if ( strcmp(jwt.nodelist, jrt.nodelist) != 0 ) {
-                printf("[%d] List of nodes differ.\n",jwt.id_job);
-            }
-            /*if ( jwt.state != jrt.state ) {
-                printf("[%d] Job state differ: %d <> %d\n",jwt.id_job, jwt.state, jrt.state);
-            }*/
-        }
-
         // makepsan
-        if (jwt.time_start < jwt_min_start) {
-            jwt_min_start = jwt.time_start;
+        if (jwt[j].time_start < jwt_min_start) {
+            jwt_min_start = jwt[j].time_start;
         }
-        if (jwt.time_end > jwt_max_end) {
-            jwt_max_end = jwt.time_end;
+        if (jwt[j].time_end > jwt_max_end) {
+            jwt_max_end = jwt[j].time_end;
         }
-        if (jrt.time_start < jrt_min_start) {
-            jrt_min_start = jrt.time_start;
+        if (jrt[j].time_start < jrt_min_start) {
+            jrt_min_start = jrt[j].time_start;
         }
-        if (jrt.time_end > jrt_max_end) {
-            jrt_max_end = jrt.time_end;
+        if (jrt[j].time_end > jrt_max_end) {
+            jrt_max_end = jrt[j].time_end;
         }
 
-        t1 = jwt.time_end - jwt.time_start;
-        t2 = jrt.time_end - jrt.time_start;
-        dist = euclidean_dist(t1,t2);
-        if (dist > threshold) {
-            printf("[%d] distance duration over threshold (%.2f)\n",jwt.id_job, dist);
+	if (f == duration) {
+		t1 = jwt[j].time_end - jwt[j].time_start;
+		t2 = jrt[j].time_end - jrt[j].time_start;
+		dist[j] = euclidean_dist(t1,t2);
+	}
+	if (f == submit) {
+        t1 = jwt[j].time_submit;
+        t2 = jrt[j].time_submit;
+        dist[j] = euclidean_dist(t1,t2);
         }
-        t1 = jwt.time_submit;
-        t2 = jrt.time_submit;
-        dist = euclidean_dist(t1,t2);
-        if (dist > threshold) {
-            printf("[%d] distance time_submit over threshold (%.2f)\n",jwt.id_job, dist);
+	if (f == eligible) {
+        t1 = jwt[j].time_eligible;
+        t2 = jrt[j].time_eligible;
+        dist[j] = euclidean_dist(t1,t2);
         }
-        t1 = jwt.time_eligible;
-        t2 = jrt.time_eligible;
-        dist = euclidean_dist(t1,t2);
-        if (dist > threshold) {
-            printf("[%d] distance time_eligible over threshold (%.2f)\n",jwt.id_job, dist);
+	if (f == start) {
+        t1 = jwt[j].time_start;
+        t2 = jrt[j].time_start;
+        dist[j] = euclidean_dist(t1,t2);
         }
-        t1 = jwt.time_start;
-        t2 = jrt.time_start;
-        dist = euclidean_dist(t1,t2);
-        if (dist > threshold) {
-            printf("[%d] distance time_start over threshold (%.2f)\n",jwt.id_job, dist);
-        }
-        t1 = jwt.time_end;
-        t2 = jrt.time_end;
-        dist = euclidean_dist(t1,t2);
-        if (dist > threshold) {
-            printf("[%d] distance time_end over threshold (%.2f)\n",jwt.id_job, dist);
+	if (f == end) {
+        t1 = jwt[j].time_end;
+        t2 = jrt[j].time_end;
+        dist[j] = euclidean_dist(t1,t2);
         }
     }
 
     printf("Makespan of workload: %ld\n", jwt_max_end - jwt_min_start);
     printf("Makespan of replay: %ld\n", jrt_max_end - jrt_min_start);
-    printf("Makespan differences: %ld\n", (jwt_max_end - jwt_min_start) -  (jrt_max_end - jrt_min_start));
+    printf("Makespan differences: %ld\n", (jrt_max_end - jrt_min_start) -  (jwt_max_end - jwt_min_start));
+    cum_field = 0;
+    for(j = 0; j < min_jobs; j++) {
+	    cum_field+=dist[j];
+    }
+    avg_field = cum_field/min_jobs;
+    cum_stddev_field = 0;
+    for(j = 0; j < min_jobs; j++) {
+        cum_stddev_field += (dist[j] - avg_field)*(dist[j] - avg_field);
+    }
+    std_field = sqrt(cum_stddev_field/min_jobs);
+    if (field_name == NULL) {
+	    field_name = strdup("submit");
+    }
+    printf("%s : %.6f %.8f\n",field_name, avg_field, std_field);
+    for(j = 0; j < min_jobs; j++) {
+	    printf(" (%d,%.6f)", jwt[j].id_job, dist[j]);
+    }
+    printf("\n");
+
+    free(dist);
+    free(jwt);
+    free(jrt);
     return 0;
 }
